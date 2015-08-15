235f1ba3dad7fe895cb2963ac21d0eb1d8eb922d
Kevin Kipp <kevin.kipp@gmail.com>
2015-08-15 14:01:43 -0500

    Add 'randex' property to Insult db class

--- dbentities.py
+++ dbentities.py
@@ -43,6 +43,7 @@ class Insult(db.Model):
     created = db.DateTimeProperty(auto_now_add = True)
     approved = db.BooleanProperty(required=True, default=False)
     reviewed = db.BooleanProperty(required=True, default=False)    
+    randex = db.FloatProperty(required=True)
     source_ip = db.StringProperty()
 
 class Blog_Post(db.Model):
